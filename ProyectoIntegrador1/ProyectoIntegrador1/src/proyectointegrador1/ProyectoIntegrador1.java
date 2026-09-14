2f335331cafc32aa6716d90696c3a0c5e1e7f578
SantiagoCampoI <katesanti06@gmail.com>
2026-09-14 11:19:06 -0500

    Login-Biblioteca_Completo
    Aquí ya esta agregada la funcionalidad del login con la biblioteca y aparte se añadió dos nuevas técnicas.

---
+++ ProyectoIntegrador1/ProyectoIntegrador1/src/proyectointegrador1/ProyectoIntegrador1.java
@@ -0,0 +1,15 @@
+package proyectointegrador1;
+
+import igu.Biblioteca;
+public class ProyectoIntegrador1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Biblioteca biblio = new Biblioteca();
+        biblio.setVisible(true);
+        biblio.setLocationRelativeTo(null);
+    }
+    
+}
